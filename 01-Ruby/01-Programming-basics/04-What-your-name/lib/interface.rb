130584b2c71d71266ff6ab9840c5035fec9d67da
Hongjoo Yang <attinore@gmail.com>
2020-09-28 23:30:37 +0900

    added questions

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,7 +1,12 @@
 require_relative "compute_name"
 
+puts "What's your first name?"
 first_name = gets.chomp
+
+puts "What's your middle name?"
 middle_name = gets.chomp
+
+puts "What's your last name?"
 last_name = gets.chomp
 
 puts "Hello, #{first_name} #{middle_name} #{last_name}"
