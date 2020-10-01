cb5dde93c5e1801753cf6eb0f5e7b780ebe2d653
Hongjoo Yang <attinore@gmail.com>
2020-10-01 22:26:40 +0900

    stupid coach messages all get correct responses

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,11 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+puts "Say something to your coach"
+your_message = gets.chomp
+
+until your_message == "I am going to work right now!"
+  puts coach_answer_enhanced(your_message)
+  puts "Say something to your coach"
+  your_message = gets.chomp
+end
