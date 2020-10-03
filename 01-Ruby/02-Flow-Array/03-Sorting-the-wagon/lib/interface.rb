83f9c3de71a1f9f669a8731efea88dc3ecc84c06
Hongjoo Yang <attinore@gmail.com>
2020-10-03 20:30:31 +0900

    create a list of students on the wagon

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -1,11 +1,16 @@
 require_relative "wagon_sort"
 
-# TODO: Ask the user about students to add to the Wagon.
-#       Remember, to read an input from the command line, use:
-#       - `gets`:  http://www.ruby-doc.org/core-2.5.3/Kernel.html#method-i-gets
-#       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
+list = []
 
+puts "Type a student name:"
+students = gets.chomp
+list.push(students) unless students == ""
 
+until students == ""
+  puts "Type another student name or press enter to finish:"
+  students = gets.chomp
+  list.push(students) unless students == ""
+end
 
-# TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
-#       file and display the sorted student list
+puts "Congratulations! Your Wagon has #{list.count} students:"
+puts "#{wagon_sort(list)[0]}, #{wagon_sort(list)[1...-1].join(', ')} and #{wagon_sort(list).last}"
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,3 @@
 def wagon_sort(students)
-  # TODO: return (not print!) a copy of students, sorted alphabetically
+  return students.sort_by(&:downcase)
 end
