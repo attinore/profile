260806e64d7c2b40f209f401cfcfba528118a1de
Hongjoo Yang <attinore@gmail.com>
2020-10-08 21:40:50 +0900

    somehow got orders from customers

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,16 @@
 def burger(patty, sauce, topping)
   # TODO: code the `burger` method
+  if block_given?
+    return ["bread", yield(patty), sauce, topping, "bread"]
+  else
+    return ["bread", patty, sauce, topping, "bread"]
+  end
 end
+
+burger("steak", "ketchup", "onions")
+
+burger("steak", "ketchup", "onions") do |patty|
+  "grilled #{patty}"
+end
+
+
