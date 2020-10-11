a50658416968f51ea4f814ac8014575b4cdfa8cb
Hongjoo Yang <attinore@gmail.com>
2020-10-11 22:18:58 +0900

    array to hash is done

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,22 @@
 def array_to_hash(array)
-  # TODO: implement the method :)
+  hash = {}
+  array.each_with_index do |element, index|
+  key =
+  if  block_given?
+    yield(index)
+  else
+    index.to_s
+  end
+  hash[key] = element
+  end
+  return hash
 end
+
+# def array_to_hash(array)
+#   hash = {}                                        # create empty hash
+#   array.each_with_index do |item, index|           # loop over array
+#     key = block_given? ? yield(index) : index.to_s # ternary to set the key in both cases
+#     hash[key] = item                               # set the new key/value pair
+#   end
+#   return hash                                      # return built hash
+# end
