d908414b8f556d236760c096cba80f9ff973b789
Hongjoo Yang <attinore@gmail.com>
2020-10-20 21:31:08 +0900

    now the vending machine works properly

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -1,5 +1,8 @@
 class VendingMachine
   # TODO: add relevant getter/setter to this class to make the scenarios work properly.
+  attr_accessor :user_balance_cents
+  attr_reader :snack_price_cents
+  attr_accessor :snack_count
 
   def initialize(snack_price_cents, snack_count)
     @user_balance_cents = 0
@@ -8,12 +11,16 @@ class VendingMachine
   end
 
   def insert_coin(input_cents)
-    # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
+    @snack_count = snack_count
+    @user_balance_cents += input_cents
+    @snack_price_cents = snack_price_cents
     # when the user inserts a coin?
   end
 
   def buy_snack
-    # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
-    # when the user pushes a button to buy a Snack?
+    if @user_balance_cents >= @snack_price_cents && @snack_count.positive?
+      @snack_count -= 1
+      @user_balance_cents -= @snack_price_cents
+    end
   end
 end
