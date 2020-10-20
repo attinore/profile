81b2766db2632d1b134998c717b8f7509d3fa42d
Hongjoo Yang <attinore@gmail.com>
2020-10-20 19:58:05 +0900

    fixed the style

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -2,4 +2,6 @@ require_relative "grocery"
 sunny_lettace = Grocery.new("Sunny Lettace", "Vegetable", 100)
 apple = Grocery.new("Fuji", "Fruit", 200)
 grocery = Grocery.new("Carrot", "Vegetable", 150)
+sunny_lettace.pack
+apple.pack
 grocery.pack
