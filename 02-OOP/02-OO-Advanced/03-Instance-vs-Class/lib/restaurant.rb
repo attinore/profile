c6268768e4feed92fa90fe2d780af5873f8bca6e
Hongjoo Yang <attinore@gmail.com>
2020-10-24 12:24:30 +0900

    the restaurant now can be rated and filtered

--- 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,22 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :city, :name, :average_rating
 
   def initialize(city, name)
+    @city = city
+    @name = name
+    @rating = []
     # TODO: implement constructor with relevant instance variables
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    # re-compute the restaurant average rating @average_rating
+    @rating << new_rate
+    @average_rating = @rating.sum(0.0) / @rating.length
+  end
+
+  def self.filter_by_city(restaurants, city)
+    filtered_restaurant = restaurants.select { |restaurant| restaurant.city == city }
+    return filtered_restaurant
+  end
 end
