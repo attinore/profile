7f321217f179cd0d831281ca1c2c361f18b4ae7c
Hongjoo Yang <attinore@gmail.com>
2020-11-13 21:03:10 +0900

    updated the stats_on and top 5

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -2,9 +2,6 @@ require 'sqlite3'
 db = SQLite3::Database.new("lib/db/jukebox.sqlite")
 
 def detailed_tracks(db)
-  # TODO: return the list of tracks with their album and artist.
-  # track title, track album name,track’s artist name.
-  # artist name and album titles.
   sql_query = <<-SQL
     SELECT t.name, al.title, ar.name
     FROM tracks t
@@ -17,22 +14,30 @@ end
 
 def stats_on(db, genre_name)
   # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
-  hash = {}
-  category = db.execute("SELECT #{genre_name} FROM genres")
-  p category
-
-  number_of_tracks = <<-SQL
-  SELECT COUNT(tracks.name) FROM tracks t
-  JOIN genres g ON g.id = t.genre_id
-  SQL
-  number_of_songs = db.execute(number_of_tracks)
-  p category
-
-  avg_millisecond = db.execute("SELECT AVG(milliseond) FROM tracks")
-  avg_length = avg_millisecond * 60_000
-  p avg_length
+  stats = db.execute(%(
+    SELECT g.name, COUNT(t.id), ROUND(AVG(milliseconds) / 60000, 2)
+    FROM tracks t
+    JOIN genres g ON g.id = t.genre_id
+    WHERE g.name = "#{genre_name}"
+    )).flatten
+  return {
+    category: stats[0],
+    number_of_songs: stats[1],
+    avg_length: stats[2]
+  }
 end
 
 def top_five_artists(db, genre_name)
+  top = db.execute(%(
+    SELECT artists.id, artists.name, COUNT(tracks.id) AS track_count
+    FROM tracks
+    JOIN albums ON albums.id = tracks.album_id
+    JOIN artists ON artists.id = albums.artist_id
+    JOIN genres ON genres.id = tracks.genre_id
+    WHERE genres.name = "#{genre_name}"
+    GROUP BY artists.id
+    ORDER BY track_count DESC
+    LIMIT 5
+    ))
   # TODO: return list of top 5 artists with the most songs for a given genre.
 end
