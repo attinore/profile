498430bc2bdf2b843e16db467e07dad8db93559a
Hongjoo Yang <attinore@gmail.com>
2020-11-17 21:41:40 +0900

    created fake data

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,11 @@
 require "faker"
+
+100.times do
+  post = Post.new(
+    title: Faker::Movies::HarryPotter,
+    url: Faker::Internet.url(host: 'example.com'),
+    votes: rand(0..10)
+  )
+  post.save!
+end
 # TODO: Write a seed to insert 100 posts in the database
