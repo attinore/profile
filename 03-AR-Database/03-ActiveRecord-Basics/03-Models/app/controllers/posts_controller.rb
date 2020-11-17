771a68eebb016e28baf48442568d7d9d1118f7af
Hongjoo Yang <attinore@gmail.com>
2020-11-17 21:03:09 +0900

    CRUD and increment supposed to be working

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -6,27 +6,44 @@ class PostsController
   end
 
   def index
+    posts = Post.all
+    @view.display_all(posts)
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
   end
 
   def create
-    # DO NOT WRITE SQL QUERIES
-    # TODO: create a post
+    title = @view.ask_user_for("title")
+    url = @view.ask_user_for("url")
+    post = Post.new(title: title, url: url)
+    post.save
   end
 
   def update
+    index
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    post.title = @view.ask_user_for("new title")
+    post.url = @view.ask_user_for("new url")
+    post.save
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
   end
 
   def destroy
+    index
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    Post.destory
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
   end
 
   def upvote
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    post.increment!(:votes, 1)
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
   end
--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,14 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def ask_user_for(stuff)
+    puts "Input #{stuff}?"
+    print "> "
+    return gets.chomp
+  end
+
+  def display_all(posts)
+    posts.each do |post|
+      puts "#{post.id}: #{post.title} (#{post.url}) - #{post.votes} vote(s))"
+    end
+  end
 end
