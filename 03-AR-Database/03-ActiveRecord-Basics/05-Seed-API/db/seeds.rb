343e989ec4da79062c9b490e1fca1d793030ee06
Hongjoo Yang <attinore@gmail.com>
2020-11-18 21:43:42 +0900

    json parsing done

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -2,3 +2,10 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+post = RestClient.get("https://hacker-news.firebaseio.com/v0/topstories.json", headers = {})
+stories = JSON.parse(post)
+10.times do
+  stories = Post.new
+  stories.save!
+  p stories
+end
