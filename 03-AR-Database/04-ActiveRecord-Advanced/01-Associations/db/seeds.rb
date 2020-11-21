518618c0b85bc55d5b98f693a11f8a6d3eb1b1e5
Hongjoo Yang <attinore@gmail.com>
2020-11-21 11:42:48 +0900

    added reference and association

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,16 @@
-# TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.new
+  user.username = Faker::Name.unique.name
+  user.email = Faker::Internet.email
+  user.save!
+  (5..10).to_a.sample.times do
+    post = Post.new(
+      title: Faker::Movies::HarryPotter,
+      url: Faker::Internet.url(host: 'example.com'),
+      user: user
+    )
+    post.save!
+  end
+end
