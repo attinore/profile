8892b6244d0afec265e7b423e54abb106389a6b5
Hongjoo Yang <attinore@gmail.com>
2020-11-21 14:58:38 +0900

    validation has been established

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
-  # TODO: Add some validation
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
+  validates :url, presence: true, format: { with: /https?:\/\/\S+/ }
+  validates :user, presence: true
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
+  before_validation :delete_spaces
+  after_create :send_email
 
-  # TODO: Add some validation
-  # TODO: Add some callbacks
+  private
+
+  def delete_spaces
+    self.email = email.strip unless email.nil?
+  end
+
+  def send_email
+    FakeMailer.instance.mail('boris@lewagon.org', 'Welcome to HN!')
+  end
 end
